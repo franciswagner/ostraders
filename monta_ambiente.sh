6489248fd6af4dd863f202a3fcfd0fb2006cc2d9
ricardococati <ricardo.cocati@ifood.com.br>
2020-02-26 12:13:15 -0300

    Alterando os mecanismos de montagem dos ambientes

--- monta_ambiente.sh
+++ monta_ambiente.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-echo "PARANDO TODAS MAQUINAS DOCKER"
-sudo docker stop $(docker ps -aq)
-
-echo "AGUARDA 2 SEGUNDOS"
-sleep 2
-
-echo "REMOVENDO TODAS MAQUINAS DOCKER"
-sudo docker rm -f $(docker ps -aq)
-
 DIR="/data/bmfCarga"
 
 if [ -d "$DIR" ]; then
@@ -33,20 +24,41 @@ else
 
 fi
 
-echo "AGUARDA 2 SEGUNDOS"
-sleep 2
+docker ps --format "{{.Names}}" | grep 'ostraders' &> /dev/null
+if [ $? == 0 ]; then
+   echo "Containers já existem no sistema operacional"
+   sudo chmod 777 /var/run/docker.sock
+   docker start --detach-keys --interactive ostraders_kafka_1
+   docker start --detach-keys --interactive ostraders_pg-dbbmf_1
+   docker start --detach-keys --interactive ostraders_pg-calcula-dbbmf_1
 
-echo "CRIANDO MÁQUINAS DOCKER(KAFKA E POSTGRESQL)"
-sudo docker-compose up -d
+else 
 
-echo "AGUARDA 5 SEGUNDOS"
-sleep 5
+	echo "PARANDO TODAS MAQUINAS DOCKER"
+	docker stop $(docker ps -aq)
 
-echo "CRIANDO TÓPICOS NO KAFKA DIARIO"
-sudo docker exec ostraders_kafka_1 /opt/kafka_2.11-0.10.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic candlestick-diario
+	echo "AGUARDA 2 SEGUNDOS"
+	sleep 2
 
-echo "AGUARDA 5 SEGUNDOS"
-sleep 5
+	echo "REMOVENDO TODAS MAQUINAS DOCKER"
+	docker rm -f $(docker ps -aq)
 
-echo "CRIANDO TÓPICOS NO KAFKA SEMANAL"
-sudo docker exec ostraders_kafka_1 /opt/kafka_2.11-0.10.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic candlestick-semanal
+	echo "AGUARDA 2 SEGUNDOS"
+	sleep 2
+
+	echo "CRIANDO MÁQUINAS DOCKER(KAFKA E POSTGRESQL)"
+	sudo docker-compose up -d
+
+	echo "AGUARDA 5 SEGUNDOS"
+	sleep 5
+
+	echo "CRIANDO TÓPICOS NO KAFKA DIARIO"
+	docker exec ostraders_kafka_1 /opt/kafka_2.11-0.10.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic candlestick-diario
+
+	echo "AGUARDA 5 SEGUNDOS"
+	sleep 5
+
+	echo "CRIANDO TÓPICOS NO KAFKA SEMANAL"
+	docker exec ostraders_kafka_1 /opt/kafka_2.11-0.10.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic candlestick-semanal
+
+fi
